65afe9d49e1dfcaacd6bd1c0ff456c22226c3fec
LuongTran2401 <mr.bean24103@gmail.com>
2025-04-25 08:07:23 +0700

    first comit

---
+++ src/gym/club/GymClub.java
@@ -0,0 +1,16 @@
+package gym.club;
+
+/**
+ *
+ * @author Thinkpad
+ */
+public class GymClub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,20 @@
+package project;
+import java.sql.*;
+
+/**
+ *
+ * @author Thinkpad
+ */
+public class ConnectionProvider {
+    public static  Connection getCon(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gms", "root", "123456");
+            return con;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
